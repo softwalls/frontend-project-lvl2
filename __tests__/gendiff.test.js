648b51537b62bbe5b10cd3658ba55ef6f3991bcf
Vyacheslav Nesmeyanov <softwalls@Vyacheslavs-Mac-mini.local>
2021-03-05 21:30:40 +0300

    make genDiff module and some tests for it. genDiff generates diff string correctly now

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -14,42 +14,71 @@ import { genDiff, getJson } from '../src/genDiff.js';
 import { readFileSync } from 'fs';
 
 // исходники для тестов
+
+const path1 = '/Users/softwalls/JS_Projects/frontend-project-lvl2/src/file1.json';
+const path2 = '/Users/softwalls/JS_Projects/frontend-project-lvl2/src/file2.json';
+const file1 = readFileSync(path1);
+const file2 = readFileSync(path2);
+
 const json1 = {
-  "host": "hexlet.io",
-  "timeout": 50
-};
-
-const json2 = {
-  "host": "hexlet.com",
-  "timeout": 50
-};
-
-const sameJson1 = {
-  "timeout": 50,
-  "host": "hexlet.io"
-}
-
-const diff = `{
-  - "host": "hexlet.io",
-  + "host": "hexlet.com",
-    "timeout": 50
-}`;
-const file1 = {
   "host": "hexlet.io",
   "timeout": 50,
   "proxy": "123.234.53.22",
   "follow": false
 };
-const path = '/Users/softwalls/JS_Projects/frontend-project-lvl2/src/file1.json';
+const json2 = {
+  "timeout": 20,
+  "verbose": true,
+  "host": "hexlet.io"
+};
+const sameJson1 = {
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+};
 
-test('shows same json if there are no diffs', () => {
-  expect(genDiff(json1, sameJson1)).toBe(JSON.stringify(json1));
-});
+const oneToTwoJson = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
 
-test('readFileSync returns file content', () => {
-  expect(JSON.parse(readFileSync(path))).toStrictEqual(file1);
-});
+const sameJson1Result = `{
+    follow: false
+    host: hexlet.io
+    proxy: 123.234.53.22
+    timeout: 50
+}`;
 
-test('getJson returns parsed json file content', () => {
-  expect(getJson(path)).toStrictEqual(file1);
-});
+const cleanedJson1 = `{
+  - follow: false
+  - host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+}`
+const emptyJson = {};
+ 
+
+
+describe('getJson', () => {
+  test('returns json from path', () => {
+    expect(getJson(path1)).toStrictEqual(json1);
+    expect(getJson(path1)).not.toStrictEqual(json2);
+  });
+})
+
+describe('genDiff', () => {
+  test('changes were not made', () => {
+    expect(genDiff(json1, sameJson1)).toBe(sameJson1Result);
+  })
+  test('some props were deleted', () => {
+    expect(genDiff(json1, emptyJson)).toStrictEqual(cleanedJson1);
+  })
+  test('some deleted, added and remained same props', () => {
+    expect(genDiff(json1, json2)).toStrictEqual(oneToTwoJson);
+  })
+})
--- src/genDiff.js
+++ src/genDiff.js
@@ -7,16 +7,48 @@ const getJson = (pathString) => {
 }
 
 const genDiff = (json1, json2) => {
-  const jsonKeys1 = Object.keys(json1).sort();
-  //const jsonKeys2 = Object.keys(json2).sort();
-  let result;
-  if (_.isEqual(json1, json2)) {
-    result = jsonKeys1.reduce((acc, key) => {
-      acc[key] = json1[key];
-      return acc;
-    }, {})
+  const getPropsToCheck = () => {
+    let resultObj = {};
+    Object.assign(resultObj, json1, json2);
+    return Object.keys(resultObj).sort();
   }
-  return JSON.stringify(result);
+  
+  const generateLineCollection = (props) => {
+    const result = props.reduce((acc, prop) => {
+      if (_.has(json1, prop) && _.has(json2, prop)) {
+        
+        //свойство не изменилось - генерится одна строка
+        if (json1[prop] === json2[prop]) {
+          
+          acc.push(`    ${prop}: ${json1[prop]}`);
+        } else {
+          //свойство именилось - генерим две строки(- и +)
+          
+          acc.push(`  - ${prop}: ${json1[prop]}`,`  + ${prop}: ${json2[prop]}`);
+        }
+      } else {
+        // свойство исчезло - генерится строка с -
+        if (_.has(json1, prop)) {
+        acc.push(`  - ${prop}: ${json1[prop]}`);
+        } else {
+        //свойство появилось - генерится строка с +
+        acc.push(`  + ${prop}: ${json2[prop]}`);
+        }
+      }
+      
+      return acc;
+    }, [])
+    return result;
+  }
+  //сюда мы передаём массив вида ['   key: value',' - key2: value2', '  + key2: value100']
+  //и получаем готовый вывод
+  const build = (linesCollection) => {
+    const result = linesCollection.join('\n');
+    return `{\n${result}\n}`;
+  }
+  const allProps = getPropsToCheck();
+  const lines = generateLineCollection(allProps);
+  return build(lines);  
 }
 
 // каждое свойство json1 сравниваем с таким же в json2
