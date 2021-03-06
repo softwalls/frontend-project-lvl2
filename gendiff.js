06372c6043e60c9d0133b3136054bd3764410026
Vyacheslav Nesmeyanov <softwalls@Vyacheslavs-Mac-mini.local>
2021-03-06 12:39:11 +0300

    fixed gendiff.js executable file (add reading path arguments feature).

--- gendiff.js
+++ gendiff.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import { genDiff, getJson } from './src/genDiff.js';
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format [type]', 'output format');
-  //.option('-h, --help', 'output usage information');
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => console
+    .log(genDiff(getJson(filepath1), getJson(filepath2))));
+  
 
 program.parse(process.argv);
 
