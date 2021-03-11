d2582ef479088307c046e60b7c0c42289df7d29f
Vyacheslav Nesmeyanov <softwalls@Vyacheslavs-Mac-mini.local>
2021-03-11 12:54:18 +0300

    provide relative link for diff file

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,22 +1,11 @@
 /* eslint-disable no-undef */
-/*
-Требования:
 
-- Сравниваются данные, а не строки файлов.
-- Две строчки дифа, отвечающие за различия поля,
-  должны находиться рядом.
-  Причем вначале должна выводиться строка относящаяся
-  к первому файлу, а затем строка относящаяся ко
-  второму файлу (см. пример с timeout).
-- Результатом работы функции genDiff() является строка.
-
-*/
 import { readFileSync } from 'fs';
 import { genDiff, getJson } from '../src/genDiff';
 
 // исходники для тестов
 
-const path1 = '/Users/softwalls/JS_Projects/frontend-project-lvl2/__fixtures__/diff';
+const path1 = './__fixtures__/diff';
 // const path2 = '/Users/softwalls/JS_Projects/frontend-project-lvl2/src/file2.json';
 // const file1 = readFileSync(path1);
 // const file2 = readFileSync(path2);
