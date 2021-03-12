3c7fce706c08b0e21ab862bb29651a3816e15178
Vyacheslav Nesmeyanov <softwalls@Vyacheslavs-Mac-mini.local>
2021-03-12 20:23:04 +0300

    create new tree-like fixtures and make some tests for tree-like files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,29 +5,37 @@ import { genDiff } from '../src/genDiff';
 
 // исходники для тестов
 
-const pathDiff = './__fixtures__/diff';
-// const path2 = '/Users/softwalls/JS_Projects/frontend-project-lvl2/src/file2.json';
-// const file1 = readFileSync(path1);
-// const file2 = readFileSync(path2);
+// const pathDiff = './__fixtures__/diff';
 
-// describe('getJson', () => {
-//   test('returns json from path', () => {
-//     expect(getJson(`file1.json`)).toStrictEqual();
-//     expect(getJson(path1)).not.toStrictEqual(json2);
-//   });
-// });
-let jsonPath1;
-let jsonPath2;
-let yamlPath1;
-let yamlPath2;
+// let jsonPath1;
+// let jsonPath2;
+// let yamlPath1;
+// let yamlPath2;
+
+const pathTreeDiff = './__fixtures__/tree_diff.txt';
+
+let treeJsonPath1;
+let treeJsonPath2;
+let treeYamlPath1;
+let treeYamlPath2;
 
 describe('genDiff', () => {
   beforeEach(() => {
-    jsonPath1 = './__fixtures__/file1.json';
-    jsonPath2 = './__fixtures__/file2.json';
-    yamlPath1 = './__fixtures__/file1.yml';
-    yamlPath2 = './__fixtures__/file2.yml';
+    // jsonPath1 = './__fixtures__/file1.json';
+    // jsonPath2 = './__fixtures__/file2.json';
+    // yamlPath1 = './__fixtures__/file1.yml';
+    // yamlPath2 = './__fixtures__/file2.yml';
+    treeJsonPath1 = './__fixtures__/tree1.json';
+    treeJsonPath2 = './__fixtures__/tree2.json';
+    treeYamlPath1 = './__fixtures__/tree1.yml';
+    treeYamlPath2 = './__fixtures__/tree2.yml';
   });
+
+  /*
+
+  // ================================================================
+  //                      ТЕСТЫ ДЛЯ ПЛОСКИХ ФАЙЛОВ
+  // ================================================================
 
   test('works fine with JSON files', () => {
     expect(genDiff(jsonPath1, jsonPath2)).toStrictEqual(`${readFileSync(pathDiff)}`);
@@ -39,5 +47,22 @@ describe('genDiff', () => {
 
   test('works with different type of files', () => {
     expect(genDiff(jsonPath1, yamlPath2)).toStrictEqual(`${readFileSync(pathDiff)}`);
+  });
+
+*/
+
+  // ================================================================
+  //                  ТЕСТЫ ДЛЯ ДРЕВОВИДНЫХ ФАЙЛОВ
+  // ================================================================
+
+  test('works with tree-like JSON files', () => {
+    expect(genDiff(treeJsonPath1, treeJsonPath2)).toStrictEqual(`${readFileSync(pathTreeDiff)}`);
+  });
+
+  test('works with tree-like YAML files', () => {
+    expect(genDiff(treeYamlPath1, treeYamlPath2)).toStrictEqual(`${readFileSync(pathTreeDiff)}`);
+  });
+  test('works with different type of tree-like files', () => {
+    expect(genDiff(treeJsonPath1, treeYamlPath2)).toStrictEqual(`${readFileSync(pathTreeDiff)}`);
   });
 });
